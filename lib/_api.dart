261905e9d2df796e7a94125f272733ab181cf741
vova-beloded <vova.beloded@solid.software>
2018-09-10 17:17:21 +0300

    Getting all videos from Youtube channel

--- lib/_api.dart
+++ lib/_api.dart
@@ -1,4 +1,4 @@
-class API{
+class API {
   String Key;
   int maxResults;
   String order;
@@ -7,55 +7,75 @@ class API{
   String videoDuration;
   String nextPageToken;
   String prevPageToken;
-  String query ;
+  String query;
   Object options;
   static String base_url = 'www.googleapis.com';
 
-  API({this.Key,this.Type,this.maxResults,this.query});
+  API({this.Key, this.Type, this.maxResults, this.query});
 
-  Uri searchUri(query,{String Type}) {
+  Uri searchUri(query, {String Type}) {
     this.query = query;
-    this.Type = Type??this.Type;
-    var options = getOtion();
-    Uri url = new Uri.https(base_url, "youtube/v3/search",options);
+    this.Type = Type ?? this.Type;
+    var options = getOption();
+    Uri url = new Uri.https(base_url, "youtube/v3/search", options);
+    return url;
+  }
+
+  Uri channelUri(String channelId, String order) {
+    this.order = order ?? 'date';
+    var options = getChannelOption(channelId, this.order);
+    Uri url = new Uri.https(base_url, "youtube/v3/search", options);
     return url;
   }
 
 //  For Getting Getting Next Page
-  Uri nextPageUri(){
-    var options = getOtions("pageToken",nextPageToken);
-    Uri url = new Uri.https(base_url, "youtube/v3/search",options);
-    return  url;
+  Uri nextPageUri() {
+    var options = getOptions("pageToken", nextPageToken);
+    Uri url = new Uri.https(base_url, "youtube/v3/search", options);
+    return url;
   }
 
 //  For Getting Getting Next Page
-  Uri prevPageUri(){
-    var options = getOtions("pageToken",prevPageToken);
-    Uri url = new Uri.https(base_url, "youtube/v3/search",options);
-    return  url;
+  Uri prevPageUri() {
+    var options = getOptions("pageToken", prevPageToken);
+    Uri url = new Uri.https(base_url, "youtube/v3/search", options);
+    return url;
   }
 
-  Object getOtions(String key, String value){
+  Object getOptions(String key, String value) {
     Object options = {
-      key:value,
+      key: value,
       "q": "${this.query}",
       "part": "snippet",
       "maxResults": "${this.maxResults}",
       "key": "${this.Key}",
-      "type":"${this.Type}"
+      "type": "${this.Type}"
     };
     return options;
   }
-  Object getOtion(){
+
+  Object getOption() {
     Object options = {
       "q": "${this.query}",
       "part": "snippet",
       "maxResults": "${this.maxResults}",
       "key": "${this.Key}",
-      "type":"${this.Type}"
+      "type": "${this.Type}"
     };
     return options;
   }
-  void setNextPageToken(String token) =>this.nextPageToken =token;
-  void setPrevPageToken(String token) =>this.nextPageToken =token;
+
+  Object getChannelOption(String channelId, String order) {
+    Object options = {
+      'channelId': channelId,
+      "part": "snippet",
+      'order': this.order,
+      "maxResults": "${this.maxResults}",
+      "key": "${this.Key}",
+    };
+    return options;
+  }
+
+  void setNextPageToken(String token) => this.nextPageToken = token;
+  void setPrevPageToken(String token) => this.nextPageToken = token;
 }
