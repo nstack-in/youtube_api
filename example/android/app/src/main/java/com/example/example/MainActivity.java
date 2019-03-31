90a07eeecfd8c4c1c4b97f9306a197ceabc5f32f
udiedrichsen <ulrich.diedrichsen@gmail.com>
2019-03-31 13:15:35 +0200

    Recreated example project

--- example/android/app/src/main/java/com/example/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/example/MainActivity.java
@@ -1,7 +1,6 @@
-package io.github.nitishk72.youtubeapi.youtubeapiexample;
+package com.example.example;
 
 import android.os.Bundle;
-
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
